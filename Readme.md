1d5518946622d5a21f7ffca638876109960ca6ee
Zach Johnson <zachjohnsondev@gmail.com>
2019-04-29 08:32:02 -0700

    add tag support, custom front matter support, lots of tests. closes #1 closes #3

--- Readme.md
+++ Readme.md
@@ -2,18 +2,18 @@
 
 Bhugo is a simple tool written in Go that transforms [Bear](https://bear.app/) notes into [Hugo](https://gohugo.io/)-compatible markdown files. It runs in the background so that whenever you make changes to your Bear notes, you can immediately see the updates on your Hugo server.
 
-Bhugo will monitor a directory of Bear notes based off of a configurable tag prefix. For example, if you prefix all your Bear notes as `#blog`  ( `#blog/finance,  #blog/life`  etc.), configure Bhugo to monitor the `blog` prefix.
+Bhugo will monitor a directory of Bear notes based off of a configurable tag prefix. For example, if you prefix all your Bear notes as `#blog`  ( `#blog/finance,  #blog/life`  etc.), configure Bhugo to monitor the `blog` prefix. Bhugo will preserve any custom front matter that you add to your Hugo files.
 
 Bhugo does it’s best to stay out of your way, with only a few requirements for how you write your notes:
 - Write your notes in markdown compatibility mode.
 - The first line of your note is treated as the title and is used to create the Hugo files and insert the title into the Hugo front matter - a note titled `My Great Post` will generate a file called `my-great-post.md`.
-- The second line of your note is expected to be hashtags (and optionally other text), which will correlate to Hugo categories in the front matter.
+- The second line of your note is expected to be hashtags (and optionally other text), which will correlate to either Hugo categories or tags in the front matter.
 - You can insert images into your Bear notes and they will be formatted to match the configurable environment variable designating the image directory in your Hugo blog - so save your images in your Hugo site as you would normally and then insert them directly into your Bear note.
 - The `draft` tag has special meaning and will specifically mark the post as a draft in the Hugo front matter, for example `#blog/draft`.
 
 - - - -
 ## **Warning**
-Bhugo will **blow away** an existing file in the `CONTENT_DIR` directory if it already exists. For example, if you title a Bear note `My New Post` and there is an existing file called `my-new-post.md` that file will be truncated and replaced with the content from your Bear note.
+Bhugo will **blow away** the body of an existing file in the `CONTENT_DIR` directory if it already exists. For example, if you title a Bear note `My New Post` and there is an existing file called `my-new-post.md` the body of that file will be truncated and replaced with the content from your Bear note. Any custom front matter in that file, however, will be preserved.
 
 - - - -
 
@@ -34,6 +34,8 @@ CONTENT_DIR=content/blog
 IMAGE_DIR=/img/posts
 NOTE_TAG=blog
 INTERVAL=1
+CATEGORIES=true
+TAGS=false
 ```
 
 Substitute your `username` in the `DATABASE` variable - this is where Bear stores it’s data. Bhugo is `read-only` on this database but if it makes you feel better, back up that file.
@@ -47,6 +49,10 @@ Substitute your `username` in the `DATABASE` variable - this is where Bear store
 `NOTE_TAG` is the tag prefix in Bear that Bhugo will monitor.
 
 `INTERVAL` is how often in seconds Bhugo will check for changes to Bear notes.
+
+`CATEGORIES` is a boolean value indicating that Bhguo will treat Bear hashtags as Hugo categories in the front matter.
+
+`TAGS` is a boolean value indicating that Bhguo will treat Bear hashtags as Hugo tags in the front matter.
 
 - - - -
 
--- main_test.go
+++ main_test.go
@@ -1,10 +1,124 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
 	"testing"
+	"text/template"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestUpdateHugo(t *testing.T) {
+	tests := []struct {
+		name    string
+		file    string
+		in      note
+		exp     []byte
+		cleanup bool
+	}{
+		{
+			"basic",
+			"note-title.md",
+			note{
+				Title: "Note Title",
+				BodyRaw: []byte(`# Note Title
+#blog/tag
+
+Body text`)},
+			[]byte(`---
+title: "Note Title"
+date: %time%
+categories: ["Tag"]
+tags: ["Tag"]
+draft: false
+---
+
+Body text`),
+			true,
+		},
+		// Should preserve custom front matter of an existing note.
+		{
+			"existing note",
+			"existing.md",
+			note{
+				Title: "Existing",
+				BodyRaw: []byte(`# Existing
+#blog/tag
+
+Updated text`)},
+			[]byte(`---
+title: "Existing"
+date: %time%
+categories: ["Tag"]
+tags: ["Tag"]
+draft: false
+custom: abc
+---
+
+Updated text`),
+			false,
+		},
+	}
+
+	now := time.Now()
+	tp := func() time.Time {
+		return now
+	}
+	tf := "2006-01-02T15:04:05-07:00"
+	tag := "blog"
+	hugoDir := "./testData/site"
+	contentDir := "content"
+	imageDir := "/"
+
+	done := make(chan bool, 2)
+	notes := make(chan note, 1)
+
+	tmpl, err := template.New("Note Template").Parse(templateRaw)
+	require.NoError(t, err)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			dir := fmt.Sprintf("%s/%s/%s", hugoDir, contentDir, test.file)
+
+			// Keep a copy of the original file if it exists.
+			orig, _ := ioutil.ReadFile(dir)
+
+			defer func() {
+				if test.cleanup {
+					err = os.Remove(dir)
+					require.NoError(t, err)
+				} else {
+					err := ioutil.WriteFile(dir, orig, 0666)
+					require.NoError(t, err)
+				}
+			}()
+
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			go updateHugo(&wg, done, notes, tp, tf, tag, hugoDir, contentDir, imageDir, tmpl, true, true)
+			notes <- test.in
+
+			// Pause for a moment to make sure the note is processed before the done channel.
+			time.Sleep(time.Millisecond)
+
+			done <- true
+			wg.Wait()
+
+			f, err := ioutil.ReadFile(dir)
+			require.NoError(t, err)
+
+			// Replace the date placeholder with the dummy timestamp.
+			exp := bytes.Replace(test.exp, []byte("%time%"), []byte(tp().Format(tf)), 1)
+
+			require.Equal(t, string(exp), string(f))
+		})
+	}
+}
 
 func TestScanTags(t *testing.T) {
 	tests := []struct {
@@ -15,7 +129,7 @@ func TestScanTags(t *testing.T) {
 		{
 			"empty",
 			[]byte(""),
-			nil,
+			[]string{},
 		},
 		{
 			"one tag",
@@ -35,7 +149,7 @@ func TestScanTags(t *testing.T) {
 		{
 			"not hashes",
 			[]byte("1234"),
-			nil,
+			[]string{},
 		},
 		{
 			"some hashes with some random text",
@@ -46,9 +160,8 @@ func TestScanTags(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			n := note{}
-			scanTags(test.in, &n, "prefix")
-			require.Equal(t, test.exp, n.Categories)
+			got := scanTags(test.in, "prefix")
+			require.Equal(t, test.exp, got)
 		})
 	}
 }
@@ -90,6 +203,63 @@ func TestParseImages(t *testing.T) {
 			for i, l := range test.exp {
 				require.Equal(t, string(l), string(test.in[i]))
 			}
+		})
+	}
+}
+
+func TestCustomFrontMatter(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []byte
+		exp  []string
+	}{
+		{"empty", nil, []string{}},
+		{
+			"basic",
+			[]byte(`---
+title: "Existing"
+date: 2019-04-29T07:55:21-07:00
+draft: false
+categories: ["blog"]
+tags: ["custom-tag"]
+custom: abc
+---
+
+Body Text`),
+			[]string{"tags: [\"custom-tag\"]", "custom: abc"},
+		},
+		{
+			"no opening dash",
+			[]byte(`title: "Existing"
+date: 2019-04-29T07:55:21-07:00
+draft: false
+categories: ["blog"]
+tags: ["custom-tag"]
+custom: abc
+---
+
+Body Text`),
+			[]string{},
+		},
+		{
+			"no closing dash",
+			[]byte(`---
+title: "Existing"
+date: 2019-04-29T07:55:21-07:00
+draft: false
+categories: ["blog"]
+tags: ["custom-tag"]
+custom: abc
+
+Body Text`),
+			[]string{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := customFrontMatter(test.in, true, false)
+			require.Equal(t, test.exp, got)
 		})
 	}
 }
